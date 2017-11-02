4ce1ba6f02b51022df429a9e4a232100e8dfdeb1
cheesecomer <orz.pgr.m9@gmail.com>
2017-11-02 14:00:59 +0900

    マイプロフィール画面 ViewModel の作成

---
+++ Capibara.Test/ViewModels/MyProfilePageViewModelTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Capibara.Models;
+using Capibara.ViewModels;
+
+using Microsoft.Practices.Unity;
+
+using Moq;
+using NUnit.Framework;
+
+using Prism.Navigation;
+
+namespace Capibara.Test.ViewModels.MyProfilePageViewModelTest
+{
+    public class NicknamePropertyTest : ViewModelTestBase
+    {
+        protected MyProfilePageViewModel Actual;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var container = this.GenerateUnityContainer();
+            container.RegisterInstance(typeof(User), UnityInstanceNames.CurrentUser, new User() { Nickname = "xxxx" });
+
+            this.Actual = new MyProfilePageViewModel().BuildUp(container);
+        }
+
+        [TestCase]
+        public void ItShouldValueWithExpect()
+        {
+            Assert.That(this.Actual.Nickname.Value, Is.EqualTo("xxxx"));
+        }
+
+        [TestCase]
+        public void ItShouldUpdate()
+        {
+            this.Actual.CurrentUser.Nickname = "xxxx!!!";
+            Assert.That(this.Actual.Nickname.Value, Is.EqualTo("xxxx!!!"));
+        }
+    }
+}
---
+++ Capibara/ViewModels/MyProfilePageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reactive.Linq;
+
+using Prism.Navigation;
+using Prism.Services;
+
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace Capibara.ViewModels
+{
+    public class MyProfilePageViewModel : ViewModelBase
+    {
+        public ReactiveProperty<string> Nickname { get; } = new ReactiveProperty<string>();
+
+        public MyProfilePageViewModel(
+            INavigationService navigationService = null,
+            IPageDialogService pageDialogService = null)
+            : base(navigationService, pageDialogService)
+        {
+        }
+
+        protected override void OnContainerChanged()
+        {
+            base.OnContainerChanged();
+
+            this.CurrentUser.ObserveProperty(x => x.Nickname).Subscribe(x => this.Nickname.Value = x);
+        }
+    }
+}
