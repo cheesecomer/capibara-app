3442d9110f6d0437c85f9b06ae12d9ec7b234109
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-05 16:31:22 +0900

    拡張ListViewの追加

---
+++ Capibara.iOS/Renderers/ExtendedListViewRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+using CoreGraphics;
+
+using Capibara.Forms;
+using Capibara.iOS.Renderers;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedListView), typeof(ExtendedListViewRenderer))]
+namespace Capibara.iOS.Renderers
+{
+    public class ExtendedListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            this.Control.Transform = new CGAffineTransform(1, 0, 0, -1, 0, 0);
+        }
+        
+    }
+}
