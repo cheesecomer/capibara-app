a8540ea1e9223400929b642f4f53eba3aabb8ffb
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-21 23:46:58 +0900

    iOS 用 AdMob バナー広告 Renderer の追加

---
+++ Capibara.iOS/PlatformVariable.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Capibara.iOS
+{
+    public static class PlatformVariable
+    {
+        public const string AdMobUnitIdForBanner = "${IOS_ADMOB_UNIT_ID_FOR_BANNER}";
+    }
+}
