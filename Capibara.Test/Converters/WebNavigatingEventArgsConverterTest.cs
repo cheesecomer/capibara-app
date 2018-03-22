dc07543ea3c20497148a57b1cf4fd8b3b371232a
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-22 17:53:26 +0900

    WebView ないでリンクから遷移した場合にブラウザを表示する

---
+++ Capibara.Test/Converters/WebNavigatingEventArgsConverterTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+using Capibara.ViewModels;
+
+using SubjectClass = Capibara.Converters.WebNavigatingEventArgsConverter;
+
+namespace Capibara.Test.Converters.WebNavigatingEventArgsConverter
+{
+    [TestFixture]
+    public class Convert
+    {
+        public void ItShouldResultIsExpect(object value, object parameter, double expect)
+        {
+            Assert.That(new SubjectClass().Convert(value, null, parameter, null), Is.TypeOf<IOverrideUrlCommandParameters>());
+        }
+    }
+
+    public class ConvertBack
+    {
+        [TestCase]
+        public void ItShouldThrowNotSupportedException()
+        {
+            Assert.Throws<NotSupportedException>(() => new SubjectClass().ConvertBack(null, null, null, null));
+        }
+    }
+}
