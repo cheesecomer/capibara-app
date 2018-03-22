8c2bc5aa5ef4f54eaa3c0829c5a33e732dc0fde2
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-22 11:52:03 +0900

    スプラッシュ画面の iPhoneX 対応

---
+++ Capibara/Converters/TopMarginConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Capibara.Converters
+{
+    public class TopMarginConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                return 0;
+            }
+
+            var size = DependencyService.Get<IScreen>().Size;
+
+            return size.Height.Equals(2436) ? 44 : 20;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
