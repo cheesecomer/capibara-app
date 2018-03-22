8dbdd8da3e25d289bd687d579b5233a08a139028
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-22 11:00:42 +0900

    BottomMarginConverter を追加し iPhoneX 環境にて下部マージンを設定

---
+++ Capibara/Converters/BottomMarginConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Capibara.Converters
+{
+    public class BottomMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                return 0;
+            }
+
+            var size = DependencyService.Get<IScreen>().Size;
+
+            return size.Height.Equals(2436) ? 34 : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
