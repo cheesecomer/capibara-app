e64c4a10750b2a32836281cecbcd4829e289f08c
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-27 08:43:01 +0900

    アプリケーションサービスの実装

---
+++ Capibara.Droid/Services/ApplicationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Capibara.Services;
+
+namespace Capibara.Droid.Services
+{
+    public class ApplicationService : IApplicationService
+    {
+        string IApplicationService.StoreUrl => string.Empty;
+
+        string IApplicationService.Platform => "Android";
+
+        string IApplicationService.AppVersion
+        {
+            get
+            {
+                var context = MainActivity.Instance.ApplicationContext;
+                return context.PackageManager.GetPackageInfo(context.PackageName, 0).VersionName;
+            }
+        }
+
+        void IApplicationService.Exit()
+            => System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+    }
+}
