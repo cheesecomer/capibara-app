91921b196505a8e644efe5f51f3d296b2e30eec0
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-27 13:41:56 +0900

    Navigating イベントが発火しない為、 WebView#Source プロパティの変更を契機にプログレスを更新

--- Capibara/Views/AcceptPage.xaml.cs
+++ Capibara/Views/AcceptPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-
+using System.Reactive.Linq;
+using Reactive.Bindings.Extensions;
 using Xamarin.Forms;
 
 namespace Capibara.Views
@@ -11,12 +11,15 @@ namespace Capibara.Views
         {
             InitializeComponent();
 
-            this.WebView.Navigating += async (s, e) =>
-            {
-                this.ProgressBar.Opacity = 1;
-                this.ProgressBar.Progress = 0;
-                await this.ProgressBar.ProgressTo(0.9, 3500, Easing.CubicOut);
-            };
+            this.WebView
+                .PropertyChangedAsObservable()
+                .Where(x => x.PropertyName == nameof(WebView.Source))
+                .Subscribe(async _ =>
+                {
+                    this.ProgressBar.Opacity = 1;
+                    this.ProgressBar.Progress = 0;
+                    await this.ProgressBar.ProgressTo(0.9, 3500, Easing.CubicOut);
+                });
 
             this.WebView.Navigated += (s, e) =>
             {
