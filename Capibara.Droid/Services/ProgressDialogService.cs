719215cc9ce4127b563814c5f4e401b55f4ba880
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-27 22:29:55 +0900

    プログレスダイアログにアルファアニメーションを追加

--- Capibara.Droid/Services/ProgressDialogService.cs
+++ Capibara.Droid/Services/ProgressDialogService.cs
@@ -1,11 +1,10 @@
-﻿using System.Threading.Tasks;
-
+﻿using System.Reactive.Disposables;
+using System.Threading.Tasks;
 using Android.Graphics;
 using Android.Views;
-
+using Android.Views.Animations;
 using Capibara.Services;
-
-using Com.Airbnb.Lottie;
+using Reactive.Bindings.Extensions;
 
 namespace Capibara.Droid.Services
 {
@@ -13,8 +12,10 @@ namespace Capibara.Droid.Services
     {
         async Task IProgressDialogService.DisplayProgressAsync(Task task, string message)
         {
+            var disposable = new CompositeDisposable();
+
             // Viewからインフレータを作成する
-            LayoutInflater layoutInflater = LayoutInflater.From(MainActivity.Instance);
+            LayoutInflater layoutInflater = LayoutInflater.From(MainActivity.Instance).AddTo(disposable);
 
             // 重ね合わせするViewの設定を行う
             var layoutParams = new WindowManagerLayoutParams(
@@ -22,26 +23,29 @@ namespace Capibara.Droid.Services
                 ViewGroup.LayoutParams.MatchParent,
                 WindowManagerTypes.Application,
                 WindowManagerFlags.WatchOutsideTouch,
-                Format.Translucent);
+                Format.Translucent)
+                .AddTo(disposable);
 
             // WindowManagerを取得する
             //wm = (WindowManager)MainActivity.Instance.GetSystemService(Context.WindowService);
             var windowManager = MainActivity.Instance.WindowManager;
 
             // レイアウトファイルから重ね合わせするViewを作成する
-            var view = layoutInflater.Inflate(Resource.Layout.overlay_progress, null);
-
-            var animationView = view.FindViewById<LottieAnimationView>(Resource.Id.animation_view);
-            //animationView
+            var view = layoutInflater.Inflate(Resource.Layout.overlay_progress, null).AddTo(disposable);
 
             // Viewを画面上に重ね合わせする
             windowManager.AddView(view, layoutParams);
 
             await task;
 
-            view.StartAnimation(new Android.Views.Animations.AlphaAnimation(.7f, 0f) { Duration = 500 });
+            var alphaAnimation = new AlphaAnimation(.7f, 0f) { Duration = 500 }.AddTo(disposable);
+            view.StartAnimation(alphaAnimation);
+
+            await Task.Delay(500);
 
             windowManager.RemoveView(view);
+
+            disposable.Dispose();
         }
     }
 }
