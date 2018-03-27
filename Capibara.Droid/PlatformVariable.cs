af280a9373dd75c0d1460d1bebc1ea1acee28f82
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-27 23:07:31 +0900

    プラットフォーム変数クラスを追加

---
+++ Capibara.Droid/PlatformVariable.cs
@@ -0,0 +1,9 @@
+﻿namespace Capibara.Droid
+{
+    public static class PlatformVariable
+    {
+        public const string AdMobUnitIdForBanner = "${ANDROID_ADMOB_UNIT_ID_FOR_BANNER}";
+
+        public const string GoogleAnalyticsTrackingId = "${GA_TRACKING_ID}";
+    }
+}
