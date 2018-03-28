d004201fb76c8b41122c6748117d3950b851c99d
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-28 10:08:58 +0900

    AdMob レンダラの実装

---
+++ Capibara.Droid/Renderers/AdMobBannerRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+using Android.Content;
+using Android.Gms.Ads;
+
+using Capibara.Forms;
+using Capibara.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(AdMobBanner), typeof(AdMobBannerRenderer))]
+namespace Capibara.Droid.Renderers
+{
+    public class AdMobBannerRenderer : ViewRenderer<AdMobBanner, AdView>
+    {
+        public AdMobBannerRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<AdMobBanner> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                var adMobBanner = new AdView(this.Context);
+                adMobBanner.AdSize = AdSize.Banner;
+                adMobBanner.AdUnitId = PlatformVariable.AdMobUnitIdForBanner;
+
+                var requestbuilder = new AdRequest.Builder();
+                //requestbuilder.te
+                adMobBanner.LoadAd(requestbuilder.Build());
+
+                this.SetNativeControl(adMobBanner);
+            }
+        }
+    }
+}
