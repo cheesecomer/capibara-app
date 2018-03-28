3728e24393e7ee67d8e3ba46081e9730b8dfb844
cheesecomer <orz.pgr.m9@gmail.com>
2018-03-28 13:18:06 +0900

    リトライ有無を返却するように変更

--- Capibara/ViewModels/ViewModelBase.cs
+++ Capibara/ViewModels/ViewModelBase.cs
@@ -126,7 +126,7 @@ namespace Capibara.ViewModels
             };
         }
 
-        protected virtual async Task DisplayErrorAlertAsync(Exception exception, Func<Task> func)
+        protected virtual async Task<bool> DisplayErrorAlertAsync(Exception exception, Func<Task> func)
         {
             if (exception is Net.HttpUnauthorizedException)
             {
@@ -162,8 +162,14 @@ namespace Capibara.ViewModels
                         "リトライ",
                         "閉じる");
 
-                if (needRetry) await func.Invoke();
+                if (needRetry)
+                {
+                    await func.Invoke();
+                    return true;
+                }
             }
+
+            return false;
         }
     }
 
