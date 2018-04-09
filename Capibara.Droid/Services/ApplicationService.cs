040136e33649d127da6943ddf68b4dec67004eda
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-09 15:09:25 +0900

    Android の UUID 取得方法を修正

--- Capibara.Droid/Services/ApplicationService.cs
+++ Capibara.Droid/Services/ApplicationService.cs
@@ -4,11 +4,13 @@ namespace Capibara.Droid.Services
 {
     public class ApplicationService : IApplicationService
     {
+        private string uuid;
+
         string IApplicationService.StoreUrl => "https://play.google.com/store/apps/details?id=com.cheesecomer.Capibara&hl=ja&ah=hTX9fjpkOefw1O8lhfrlt5IHbyM";
 
         string IApplicationService.Platform => "Android";
 
-        string IApplicationService.UUID => Android.Provider.Settings.Secure.AndroidId;
+        string IApplicationService.UUID => this.uuid.Presence() ?? (this.uuid = Android.Provider.Settings.Secure.GetString(MainActivity.Instance.ContentResolver, Android.Provider.Settings.Secure.AndroidId));
 
         string IApplicationService.AppVersion
         {
