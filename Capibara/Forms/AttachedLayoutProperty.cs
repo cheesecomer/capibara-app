f62df93e14f6ff83a71a7ce809ff7c3962de5381
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-10 11:48:56 +0900

    ItemsControl の実装

---
+++ Capibara/Forms/AttachedLayoutProperty.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+namespace Capibara.Forms
+{
+    public static class AttachedLayoutProperty
+    {
+        public static readonly BindableProperty IsItemsHostProperty =
+            BindableProperty.CreateAttached("IsItemsHost", typeof(bool), typeof(Layout<View>), false);
+
+        public static bool GetIsItemsHost(Layout<View> layout)
+        {
+            return (bool)layout.GetValue(IsItemsHostProperty);
+        }
+
+        public static void SetIsItemsHost(Layout<View> layout, bool value)
+        {
+            layout.SetValue(IsItemsHostProperty, value);
+        }
+    }
+}
