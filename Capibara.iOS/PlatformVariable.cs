6c5e4150da921e3b4c5be1ece0ef5ac6d080b401
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-12 15:56:16 +0900

    AdMob アプリケーションID の設定

--- Capibara.iOS/PlatformVariable.cs
+++ Capibara.iOS/PlatformVariable.cs
@@ -3,6 +3,8 @@ namespace Capibara.iOS
 {
     public static class PlatformVariable
     {
+        public const string AdMobApplicationId = "${IOS_ADMOB_APP_ID}";
+
         public const string AdMobUnitIdForBanner = "${IOS_ADMOB_UNIT_ID_FOR_BANNER}";
 
         public const string AdMobUnitIdForRewardedVideo = "${IOS_ADMOB_UNIT_ID_FOR_REWARDED_VIDEO}";
