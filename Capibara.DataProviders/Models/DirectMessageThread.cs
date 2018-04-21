3dfc2e27e9452507186a5beed3216fe3b31291c7
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-21 20:58:16 +0900

    api/direct_messages のクライアントを作成

---
+++ Capibara.DataProviders/Models/DirectMessageThread.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace Capibara.Models
+{
+    public class DirectMessageThread : ModelBase<DirectMessageThread>
+    {
+        private DirectMessage latestDirectMessage;
+
+        private User user;
+
+        [JsonProperty("latest_direct_message")]
+        public DirectMessage LatestDirectMessage
+        {
+            get => this.latestDirectMessage;
+            set => this.SetProperty(ref this.latestDirectMessage, value);
+        }
+
+        public User User
+        {
+            get => this.user;
+            set => this.SetProperty(ref this.user, value);
+        }
+    }
+}
---
+++ Capibara.DataProviders/Net/DirectMessages/IndexResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Capibara.Models;
+
+namespace Capibara.Net.DirectMessages
+{
+    public class IndexResponse
+    {
+        List<DirectMessageThread> Threads { get; set; }
+    }
+}
