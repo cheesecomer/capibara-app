f64bb36fa738b1e90c39fbe2247981b5deb5488c
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-22 19:08:28 +0900

    テストケース追加

--- Capibara.DataProviders.Test/Models/MessageTest.cs
+++ Capibara.DataProviders.Test/Models/MessageTest.cs
@@ -28,7 +28,7 @@ namespace Capibara.Test.Models.MessageTest
             }
 
             [TestCase]
-            public void ItShouldNameWithExpected()
+            public void ItShouldContentWithExpected()
             {
                 Assert.That(this.Subject.Content, Is.EqualTo("FooBar. Yes!Yes!Yeeeeees!"));
             }
@@ -76,7 +76,7 @@ namespace Capibara.Test.Models.MessageTest
             }
 
             [TestCase]
-            public void ItShouldNameWithExpected()
+            public void ItShouldContentWithExpected()
             {
                 Assert.That(this.Subject.Content, Is.EqualTo("FooBar. Yes!Yes!Yeeeeees!"));
             }
@@ -124,7 +124,7 @@ namespace Capibara.Test.Models.MessageTest
             }
 
             [TestCase]
-            public void ItShouldNameWithExpected()
+            public void ItShouldContentWithExpected()
             {
                 Assert.That(this.Subject.Content, Is.EqualTo("FooBar. Yes!Yes!Yeeeeees!"));
             }
@@ -151,6 +151,60 @@ namespace Capibara.Test.Models.MessageTest
             public void ItShouldIsOwnWithExpected()
             {
                 Assert.That(this.Subject.IsOwn, Is.EqualTo(false));
+            }
+        }
+
+        [TestFixture]
+        public class WhenHasImage : TestFixtureBase
+        {
+            private Message Subject;
+
+            [SetUp]
+            public override void SetUp()
+            {
+                base.SetUp();
+
+                var json = "{ \"sender\": { \"id\": 10, \"nickname\": \"ABC\" }, \"id\": 99999, \"image_url\": \"http://unit-test.com/example.jpg\", \"image_thumb_url\": \"http://unit-test.com/thumb-example.jpg\", \"at\":  \"2017-10-28T20:25:20.000+09:00\" }";
+                this.Subject = JsonConvert.DeserializeObject<Message>(json);
+
+                this.Subject.BuildUp(this.Container);
+                this.IsolatedStorage.UserId = 10;
+            }
+
+            [TestCase]
+            public void ItShouldImageUrlWithExpected()
+            {
+                Assert.That(this.Subject.ImageUrl, Is.EqualTo("http://unit-test.com/example.jpg"));
+            }
+
+            [TestCase]
+            public void ItShouldImageThumbnailUrlWithExpected()
+            {
+                Assert.That(this.Subject.ImageThumbnailUrl, Is.EqualTo("http://unit-test.com/thumb-example.jpg"));
+            }
+
+            [TestCase]
+            public void ItShouldIdWithExpected()
+            {
+                Assert.That(this.Subject.Id, Is.EqualTo(99999));
+            }
+
+            [TestCase]
+            public void ItShouldAtWithExpected()
+            {
+                Assert.That(this.Subject.At, Is.EqualTo(new DateTimeOffset(2017, 10, 28, 20, 25, 20, TimeSpan.FromHours(9))));
+            }
+
+            [TestCase]
+            public void ItShouldSenderWithExpected()
+            {
+                Assert.That(this.Subject.Sender, Is.EqualTo(new User { Id = 10, Nickname = "ABC" }).Using(new UserComparer()));
+            }
+
+            [TestCase]
+            public void ItShouldIsOwnWithExpected()
+            {
+                Assert.That(this.Subject.IsOwn, Is.EqualTo(true));
             }
         }
     }
