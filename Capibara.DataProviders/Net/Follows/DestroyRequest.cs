90325b63aefcb9435b3e24f34812d7793eef1609
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-22 19:23:23 +0900

    不要なメソッドを削除

--- Capibara.DataProviders/Net/Follows/DestroyRequest.cs
+++ Capibara.DataProviders/Net/Follows/DestroyRequest.cs
@@ -16,9 +16,6 @@ namespace Capibara.Net.Follows
 
         public override bool NeedAuthentication { get; } = true;
 
-        public override string StringContent
-            => JsonConvert.SerializeObject(this);
-
         public override string ContentType { get; } = "application/json";
 
         public DestroyRequest(int followId)
