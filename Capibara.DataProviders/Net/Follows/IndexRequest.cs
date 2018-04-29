1b5d2f4be32214a26cfb8501cc0320738f94c4c3
cheesecomer <orz.pgr.m9@gmail.com>
2018-04-30 02:07:14 +0900

    フォロー一覧取得APIクライアントの実装

---
+++ Capibara.DataProviders/Net/Follows/IndexRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Capibara.Net.Follows
+{
+    /// <summary>
+    /// GET API_BASE/follows へのリクエスト
+    /// </summary>
+    public class IndexRequest : RequestBase<IndexResponse>
+    {
+        public override HttpMethod Method { get; } = HttpMethod.Get;
+
+        public override bool NeedAuthentication { get; } = true;
+
+        public override string[] Paths { get; } = new string[] { "follows" };
+    }
+}
