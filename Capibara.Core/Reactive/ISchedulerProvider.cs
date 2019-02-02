f48c8b1964ef9da65eb9cd602f8f61bdd8f0f2e0
cheesecomer <orz.pgr.m9@gmail.com>
2019-02-02 18:10:41 +0900

    Reactive スケジューラープロバイダインターフェイスを追加

---
+++ Capibara.Core/Reactive/ISchedulerProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Reactive.Concurrency;
+
+namespace Capibara.Reactive
+{
+    public interface ISchedulerProvider
+    {
+        IScheduler UI { get; }
+
+        IScheduler Delay { get; }
+
+        IScheduler IO { get; }
+    }
+}
