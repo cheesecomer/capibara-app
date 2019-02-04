961bc171178132b0fed3717f6dd2b5c82f2a214a
cheesecomer <orz.pgr.m9@gmail.com>
2019-02-04 18:13:41 +0900

    View にボーダーを設定するエフェクトを追加

---
+++ Capibara.Presentation/Forms/Effects/Border.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+
+namespace Capibara.Presentation.Forms.Effects
+{
+    public class BorderRoutingEffect : RoutingEffect
+    {
+        public BorderRoutingEffect() : base("Capibara.Effects." + nameof(Border)) { }
+    }
+
+    public static class Border
+    {
+        #region BindableProperty
+        public static readonly BindableProperty RadiusProperty =
+            BindableProperty.CreateAttached(
+                "Radius",
+                typeof(double),
+                typeof(Border),
+                default(double),
+                propertyChanged: OnApply);
+
+        public static readonly BindableProperty WidthProperty =
+            BindableProperty.CreateAttached(
+                "Width",
+                typeof(double),
+                typeof(Border),
+                default(double),
+                propertyChanged: OnApply);
+
+        public static readonly BindableProperty ColorProperty =
+            BindableProperty.CreateAttached(
+                "Color",
+                typeof(Color),
+                typeof(Border),
+                Color.Transparent,
+                propertyChanged: OnApply);
+        #endregion
+
+        public static void SetRadius(BindableObject view, double value)
+        {
+            view.SetValue(RadiusProperty, value);
+        }
+
+        public static double GetRadius(BindableObject view)
+        {
+            return (double)view.GetValue(RadiusProperty);
+        }
+
+        public static void SetWidth(BindableObject view, double value)
+        {
+            view.SetValue(WidthProperty, value);
+        }
+
+        public static double GetWidth(BindableObject view)
+        {
+            return (double)view.GetValue(WidthProperty);
+        }
+
+        public static void SetColor(BindableObject view, Color value)
+        {
+            view.SetValue(ColorProperty, value);
+        }
+
+        public static Color GetColor(BindableObject view)
+        {
+            return (Color)view.GetValue(ColorProperty);
+        }
+
+        private static void OnApply(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is View view)
+            {
+                if (view == null)
+                    return;
+
+                var attachedEffect = view.Effects.FirstOrDefault(e => e is BorderRoutingEffect);
+                if (attachedEffect == null)
+                {
+                    view.Effects.Add(new BorderRoutingEffect());
+                }
+            }
+        }
+    }
+}
---
+++ Capibara.Test.Presentation/Forms/Effects/BorderTest.cs
@@ -0,0 +1,155 @@
+﻿#pragma warning disable CS1701 // アセンブリ参照が ID と一致すると仮定します
+using System;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.Forms;
+
+namespace Capibara.Presentation.Forms.Effects
+{
+    public class BorderTest
+    {
+        [Test]
+        public void SetWidth_WhenBeforeSet()
+        {
+            var view = new ContentView();
+
+            Assert.That(view.Effects.Count(x => x is BorderRoutingEffect), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SetWidth_WhenAfterSet()
+        {
+            var view = new ContentView();
+
+            Border.SetWidth(view, 1);
+
+            Assert.That(view.Effects.Count(x => x is BorderRoutingEffect), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SetWidth_WhenMany()
+        {
+            var view = new ContentView();
+
+            Border.SetWidth(view, 1);
+            Border.SetWidth(view, 1);
+            Border.SetWidth(view, 1);
+
+            Assert.That(view.Effects.Count(x => x is BorderRoutingEffect), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetWidth_WhenBeforeSet()
+        {
+            var view = new ContentView();
+
+            Assert.That(Border.GetWidth(view), Is.EqualTo(0d));
+        }
+
+        [Test]
+        public void GetWidth_WhenAfterSet()
+        {
+            var view = new ContentView();
+
+            Border.SetWidth(view, 1);
+
+            Assert.That(Border.GetWidth(view), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SetRadius_WhenBeforeSet()
+        {
+            var view = new ContentView();
+
+            Assert.That(view.Effects.Count(x => x is BorderRoutingEffect), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SetRadius_WhenAfterSet()
+        {
+            var view = new ContentView();
+
+            Border.SetRadius(view, 1);
+
+            Assert.That(view.Effects.Count(x => x is BorderRoutingEffect), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SetRadius_WhenMany()
+        {
+            var view = new ContentView();
+
+            Border.SetRadius(view, 1);
+            Border.SetRadius(view, 1);
+            Border.SetRadius(view, 1);
+
+            Assert.That(view.Effects.Count(x => x is BorderRoutingEffect), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetRadius_WhenBeforeSet()
+        {
+            var view = new ContentView();
+
+            Assert.That(Border.GetRadius(view), Is.EqualTo(0d));
+        }
+
+        [Test]
+        public void GetRadius_WhenAfterSet()
+        {
+            var view = new ContentView();
+
+            Border.SetRadius(view, 1);
+
+            Assert.That(Border.GetRadius(view), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SetColor_WhenBeforeSet()
+        {
+            var view = new ContentView();
+
+            Assert.That(view.Effects.Count(x => x is BorderRoutingEffect), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SetColor_WhenAfterSet()
+        {
+            var view = new ContentView();
+
+            Border.SetColor(view, Color.Red);
+
+            Assert.That(view.Effects.Count(x => x is BorderRoutingEffect), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SetColor_WhenMany()
+        {
+            var view = new ContentView();
+
+            Border.SetColor(view, Color.Red);
+            Border.SetColor(view, Color.Red);
+            Border.SetColor(view, Color.Red);
+
+            Assert.That(view.Effects.Count(x => x is BorderRoutingEffect), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetColor_WhenBeforeSet()
+        {
+            var view = new ContentView();
+
+            Assert.That(Border.GetColor(view), Is.EqualTo(Color.Transparent));
+        }
+
+        [Test]
+        public void GetColor_WhenAfterSet()
+        {
+            var view = new ContentView();
+
+            Border.SetColor(view, Color.Red);
+
+            Assert.That(Border.GetColor(view), Is.EqualTo(Color.Red));
+        }
+    }
+}
