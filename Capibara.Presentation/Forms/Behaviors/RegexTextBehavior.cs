9f220a216753157a7e63b5df848794b31c6241b9
cheesecomer <orz.pgr.m9@gmail.com>
2019-02-04 17:38:18 +0900

    正規表現ビヘイビアを追加

---
+++ Capibara.Presentation/Forms/Behaviors/RegexTextBehavior.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace Capibara.Presentation.Forms.Behaviors
+{
+    public class RegexTextBehavior : Behavior<Entry>
+    {
+        /// <summary>
+        /// 正規表現
+        /// </summary>
+        private Regex reg;
+
+        private string regexPattern;
+
+        /// <summary>
+        /// 正規表現パターン
+        /// </summary>
+        public string RegexPattern 
+        {
+            get { return this.regexPattern; } 
+            set { this.reg = new Regex(this.regexPattern = value); } 
+        }
+
+        /// <summary>
+        /// Attached
+        /// </summary>
+        /// <param name="bindable"></param>
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += TextChanged;
+        }
+
+        /// <summary>
+        /// Detaching
+        /// </summary>
+        /// <param name="bindable"></param>
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= TextChanged;
+        }
+
+        /// <summary>
+        /// 入力テキスト変更時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        internal void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.NewTextValue))
+            {
+                ((Entry)sender).Text = string.Empty;
+            }
+            else if (!(this.reg?.IsMatch(e.NewTextValue ?? string.Empty)).GetValueOrDefault())
+            {
+                // 正規表現にマッチしない場合、変更前の値に戻す
+                // 以前の値が正規表現にマッチする場合は、以前の値
+                // 以前の値が正規表現にマッチしない場合、string.Empty
+                if ((this.reg?.IsMatch(e.OldTextValue ?? string.Empty)).GetValueOrDefault())
+                {
+                    ((Entry)sender).Text = e.OldTextValue ?? string.Empty;
+                }
+                else
+                {
+                    ((Entry)sender).Text = string.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ Capibara.Test.Presentation/Forms/Behaviors/RegexTextBehaviorTest.cs
@@ -0,0 +1,52 @@
+﻿#pragma warning disable CS1701 // アセンブリ参照が ID と一致すると仮定します
+using NUnit.Framework;
+using Xamarin.Forms;
+
+namespace Capibara.Presentation.Forms.Behaviors
+{
+    public class RegexTextBehaviorTest
+    {
+        [Test]
+        public void OnAttachedTo()
+        {
+            var entry = new Entry();
+            var subject = new RegexTextBehavior();
+
+            Assert.DoesNotThrow(() => entry.Behaviors.Add(subject));
+        }
+
+        [Test]
+        public void OnDetachingFrom()
+        {
+            var entry = new Entry();
+            var subject = new RegexTextBehavior();
+
+            entry.Behaviors.Add(subject);
+            Assert.DoesNotThrow(() => entry.Behaviors.Remove(subject));
+        }
+
+        [Test]
+        [TestCase(null, "1234567890", "1234567890", TestName = "OnTextChange WhenNullToValid ShouldChange")]
+        [TestCase(null, "ABCDEFGHIJ", "", TestName = "OnTextChange WhenNullToInvalid ShouldEmpty")]
+        [TestCase(null, "", "", TestName = "OnTextChange WhenNullToEmpty ShouldEmpty")]
+        [TestCase("", "1234567890", "1234567890", TestName = "OnTextChange WhenEmptyToValid ShouldChange")]
+        [TestCase("", "ABCDEFGHIJ", "", TestName = "OnTextChange WhenEmptyToInvalid ShouldEmpty")]
+        [TestCase("", null, "", TestName = "OnTextChange WhenEmptyToNull ShouldEmpty")]
+        [TestCase("1234567890", "0987654321", "0987654321", TestName = "OnTextChange WhenValidToValid ShouldChange")]
+        [TestCase("1234567890", "ABCDEFGHIJ", "1234567890", TestName = "OnTextChange WhenValidToInvalid ShouldRollback")]
+        [TestCase("1234567890", "", "", TestName = "OnTextChange WhenValidToEmpty ShouldEmpty")]
+        [TestCase("1234567890", null, "", TestName = "OnTextChange WhenValidToNull ShouldEmpty")]
+        [TestCase("ABCDEFGHIJ", "1234567890", "1234567890", TestName = "OnTextChange WhenInvalidToValid ShouldChange")]
+        [TestCase("ABCDEFGHIJ", "KLMNOPQRST", "", TestName = "OnTextChange WhenInvalidToInvalid ShouldEmpty")]
+        [TestCase("ABCDEFGHIJ", "", "", TestName = "OnTextChange WhenInvalidToEmpty ShouldEmpty")]
+        [TestCase("ABCDEFGHIJ", null, "", TestName = "OnTextChange WhenInvalidToNull ShouldEmpty")]
+        public void OnTextChange_InvalidToInvalid(string before, string after, string expected)
+        {
+            var entry = new Entry { Text = before };
+            entry.Behaviors.Add(new RegexTextBehavior { RegexPattern = "^[0-9]*$" });
+
+            entry.Text = after;
+            Assert.That(entry.Text, Is.EqualTo(expected));
+        }
+    }
+}
