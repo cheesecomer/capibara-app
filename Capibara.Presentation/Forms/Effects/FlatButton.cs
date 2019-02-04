d40559aaaf72272801ffbb847ea0b294d926f355
cheesecomer <orz.pgr.m9@gmail.com>
2019-02-04 18:13:25 +0900

    平坦なボタンに変換するエフェクトを追加

---
+++ Capibara.Presentation/Forms/Effects/FlatButton.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+
+namespace Capibara.Presentation.Forms.Effects
+{
+    public class FlatButtonRoutingEffect : RoutingEffect
+    {
+        public FlatButtonRoutingEffect() : base("Capibara.Presentation.Forms.Effects." + nameof(FlatButton)) { }
+    }
+
+    public static class FlatButton
+    {
+        public static readonly BindableProperty EnableProperty =
+            BindableProperty.CreateAttached(
+                    "Enable",
+                    typeof(bool),
+                    typeof(FlatButton),
+                    false,
+                    propertyChanged: EnableChanged);
+
+        public static readonly BindableProperty RippleColorProperty =
+            BindableProperty.CreateAttached(
+                    "RippleColor",
+                    typeof(Color),
+                    typeof(FlatButton),
+                    default(Color));
+
+        public static void SetEnable(BindableObject view, bool value)
+        {
+            view.SetValue(EnableProperty, value);
+        }
+
+        public static bool GetEnable(BindableObject view)
+        {
+            return (bool)view.GetValue(EnableProperty);
+        }
+
+        public static void SetRippleColor(BindableObject view, Color value)
+        {
+            view.SetValue(RippleColorProperty, value);
+        }
+
+        public static Color GetRippleColor(BindableObject view)
+        {
+            return (Color)view.GetValue(RippleColorProperty);
+        }
+
+        private static void EnableChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is View view)
+            {
+                if (!(view is Button)) return;
+                
+                if ((bool)newValue)
+                {
+                    view.Effects.Add(new FlatButtonRoutingEffect());
+                }
+                else
+                {
+                    var toRemove = view.Effects.FirstOrDefault(e => e is FlatButtonRoutingEffect);
+                    if (toRemove != null)
+                        view.Effects.Remove(toRemove);
+                }
+            }
+        }
+    }
+}
---
+++ Capibara.Test.Presentation/Forms/Effects/FlatButtonTest.cs
@@ -0,0 +1,92 @@
+﻿#pragma warning disable CS1701 // アセンブリ参照が ID と一致すると仮定します
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.Forms;
+
+namespace Capibara.Presentation.Forms.Effects
+{
+    public class FlatButtonTest
+    {
+        [Test]
+        public void SetEnable_WhenBeforeSet()
+        {
+            var view = new Button();
+
+            Assert.That(view.Effects.Count(x => x is FlatButtonRoutingEffect), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SetEnable_WhenAfterSet()
+        {
+            var view = new Button();
+
+            FlatButton.SetEnable(view, true);
+
+            Assert.That(view.Effects.Count(x => x is FlatButtonRoutingEffect), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SetEnable_WhenNotButton()
+        {
+            var view = new ContentView();
+
+            FlatButton.SetEnable(view, true);
+
+            Assert.That(view.Effects.Count(x => x is FlatButtonRoutingEffect), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetEnable_WhenBeforeSet()
+        {
+            var view = new Button();
+
+            Assert.That(FlatButton.GetEnable(view), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void GetEnable_WhenAfterSet()
+        {
+            var view = new Button();
+
+            FlatButton.SetEnable(view, true);
+
+            Assert.That(FlatButton.GetEnable(view), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void SetRippleColor_WhenBeforeSet()
+        {
+            var view = new Button();
+
+            Assert.That(view.Effects.Count(x => x is FlatButtonRoutingEffect), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SetRippleColor_WhenAfterSet()
+        {
+            var view = new Button();
+
+            FlatButton.SetRippleColor(view, Color.Red);
+
+            Assert.That(view.Effects.Count(x => x is FlatButtonRoutingEffect), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetRippleColor_WhenBeforeSet()
+        {
+            var view = new Button();
+
+            Assert.That(FlatButton.GetRippleColor(view), Is.EqualTo(default(Color)));
+        }
+
+        [Test]
+        public void GetRippleColor_WhenAfterSet()
+        {
+            var view = new Button();
+
+            FlatButton.SetRippleColor(view, Color.Red);
+
+            Assert.That(FlatButton.GetRippleColor(view), Is.EqualTo(Color.Red));
+        }
+    }
+}
