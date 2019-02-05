f593c9b1acac60c6b228b749369e60e286b6fead
cheesecomer <orz.pgr.m9@gmail.com>
2019-02-05 17:22:21 +0900

    NavigationParametersExtensions のテストコードを追加

---
+++ Capibara.Test/Presentation/NavigationParametersExtensionsTest.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS1701 // アセンブリ参照が ID と一致すると仮定します
+using NUnit.Framework;
+using Prism.Navigation;
+
+namespace Capibara.Presentation
+{
+    public class NavigationParametersExtensionsTest
+    {
+        [Test]
+        [TestCase("Foo", "Bar", TestName = "TryGetValue WhenExistsKey ShouldReturnValue")]
+        [TestCase("Foo!", null, TestName = "TryGetValue WhenNotExistsKey ShouldReturnDefaultValue")]
+        public void TryGetValue(string key, string expected)
+        {
+            Assert.That(new NavigationParameters { { "Foo", "Bar" } }.TryGetValue<string>(key), Is.EqualTo(expected));
+        }
+    }
+}
