2da294468f7ed6dba29817aac84c40d0502c7113
cheesecomer <orz.pgr.m9@gmail.com>
2019-02-06 15:00:54 +0900

    Faker.Net の拡張ライブラリを追加

---
+++ Faker.Extension/Url.cs
@@ -0,0 +1,9 @@
+﻿namespace Faker
+{
+    public static class Url
+    {
+        public static string Root() => $"https://{Internet.DomainName()}.com/";
+
+        public static string Image() => $"https://{Internet.DomainName()}.com/images/{System.Guid.NewGuid()}.png";
+    }
+}
