c0359e1b6c77907a5300a199d7c9a5743660a0ce
cheesecomer <orz.pgr.m9@gmail.com>
2019-02-06 17:30:16 +0900

    未使用の using を削除

--- Capibara.Test/Presentation/ViewModels/FloorMapPageViewModelTest.cs
+++ Capibara.Test/Presentation/ViewModels/FloorMapPageViewModelTest.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Subjects;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Capibara.Domain.Models;
