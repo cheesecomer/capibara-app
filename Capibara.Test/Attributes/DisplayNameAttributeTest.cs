dd27f17ba92b03b81ff2da56ade22e7396834686
cheesecomer <orz.pgr.m9@gmail.com>
2019-02-06 19:15:44 +0900

    DisplayNameAttribute のテストコードを追加

---
+++ Capibara.Test/Attributes/DisplayNameAttributeTest.cs
@@ -0,0 +1,15 @@
+﻿#pragma warning disable CS1701 // アセンブリ参照が ID と一致すると仮定します
+using NUnit.Framework;
+
+namespace Capibara.Attributes
+{
+    public class DisplayNameAttributeTest
+    {
+        [Test]
+        public void DisplayName()
+        {
+            var expected = Faker.Lorem.Sentence();
+            Assert.That(new DisplayNameAttribute(expected).DisplayName, Is.EqualTo(expected));
+        }
+    }
+}
