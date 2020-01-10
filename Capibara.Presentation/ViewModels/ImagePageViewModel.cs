0021a340320568fa5b67543fa5d9ca21e21e38e1
cheesecomer <orz.pgr.m9@gmail.com>
2020-01-10 16:51:40 +0900

    Add ImagePageViewModel

---
+++ Capibara.Presentation/ViewModels/ImagePageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Capibara.Presentation.Navigation;
+using Prism.Navigation;
+using Prism.Services;
+using Reactive.Bindings;
+
+namespace Capibara.Presentation.ViewModels
+{
+    public class ImagePageViewModel : ViewModelBase
+    {
+        public ReactiveProperty<string> ImageUrl { get; } = new ReactiveProperty<string>();
+
+        public ImagePageViewModel(
+            INavigationService navigationService = null,
+            IPageDialogService pageDialogService = null)
+            : base(navigationService, pageDialogService) { }
+
+        public override void Initialize(INavigationParameters parameters)
+        {
+            base.Initialize(parameters);
+
+            var url = parameters.TryGetValue<string>(ParameterNames.Url);
+            if (url.IsPresent())
+            {
+                this.ImageUrl.Value = url;
+            }
+        }
+    }
+}
---
+++ Capibara.Test.Presentation/ViewModels/ImagePageViewModelTest.cs
@@ -0,0 +1,43 @@
+﻿#pragma warning disable CS1701 // アセンブリ参照が ID と一致すると仮定します
+using System;
+using System.Collections;
+using System.Reactive.Linq;
+using Capibara.Presentation.Navigation;
+using Moq;
+using NUnit.Framework;
+
+namespace Capibara.Presentation.ViewModels
+{
+    [TestFixture]
+    public class ImagePageViewModelTest
+    {
+        #region Initialize
+
+        public static IEnumerable Initialize_TestCaseSource()
+        {
+            yield return new TestCaseData(string.Empty)
+                .SetName("Initialize when url empty should ImageUrl is null");
+
+            yield return new TestCaseData(null)
+                .SetName("Initialize when url null should ImageUrl is null");
+
+
+            yield return new TestCaseData(Faker.Url.Root())
+                .SetName("Initialize when url present should ImageUrl is present");
+        }
+
+        [Test]
+        [TestCaseSource("Initialize_TestCaseSource")]
+        public void InitializeTest(string imageUrl)
+        {
+            var viewModel = new ImagePageViewModel();
+            var navigationParameter = new NavigationParameterBuilder { Url = imageUrl }.Build();
+
+            viewModel.Initialize(navigationParameter);
+
+            Assert.That(viewModel.ImageUrl.Value, Is.EqualTo(imageUrl.Presence()));
+        }
+
+        #endregion
+    }
+}
