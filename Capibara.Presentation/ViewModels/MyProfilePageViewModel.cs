f5b924119ec0f766af8411f006f1291155690340
cheesecomer <orz.pgr.m9@gmail.com>
2020-01-11 17:01:07 +0900

    Add MyProfilePageViewModel

---
+++ Capibara.Presentation/ViewModels/MyProfilePageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Capibara.Domain.Models;
+using Capibara.Presentation.Navigation;
+
+using Prism.Navigation;
+using Prism.Services;
+
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace Capibara.Presentation.ViewModels
+{
+    public class MyProfilePageViewModel : UserViewModel
+    {
+        public AsyncReactiveCommand EditCommand { get; }
+
+        public AsyncReactiveCommand SettingCommand { get; }
+
+        public AsyncReactiveCommand ShowFriendsCommand { get; }
+
+        public ReactiveProperty<int> FriendCount { get; }
+
+        public MyProfilePageViewModel(
+            INavigationService navigationService = null,
+            IPageDialogService pageDialogService = null,
+            User model = null)
+            : base(navigationService, pageDialogService, model)
+        {
+            this.FriendCount = this.Model
+                .ToReactivePropertyAsSynchronized(x => x.FriendsCount)
+                .AddTo(this.Disposable);
+
+            this.EditCommand = new AsyncReactiveCommand()
+                .WithSubscribe(this.NavigateToEditProfilePageAsync)
+                .AddTo(this.Disposable);
+
+            this.SettingCommand = new AsyncReactiveCommand()
+                .WithSubscribe(this.NavigateToSettingPageAsync)
+                .AddTo(this.Disposable);
+
+            this.ShowFriendsCommand = new AsyncReactiveCommand()
+                .WithSubscribe(this.NavigateToFollowUsersPageAsync)
+                .AddTo(this.Disposable);
+        }
+
+        private Task NavigateToEditProfilePageAsync()
+        {
+            var parameters = new NavigationParameterBuilder { Model = this.Model }.Build();
+            return this.NavigationService.NavigateAsync("EditProfilePage", parameters);
+        }
+
+        private Task NavigateToSettingPageAsync()
+            => this.NavigationService.NavigateAsync("SettingPage");
+
+        private Task NavigateToFollowUsersPageAsync()
+            => this.NavigationService.NavigateAsync("FollowUsersPage");
+    }
+}
---
+++ Capibara.Test.Presentation/ViewModels/MyProfilePageViewModelTest.cs
@@ -0,0 +1,82 @@
+﻿#pragma warning disable CS1701 // アセンブリ参照が ID と一致すると仮定します
+using Capibara.Presentation.Navigation;
+using Moq;
+using NUnit.Framework;
+using Prism.Navigation;
+
+namespace Capibara.Presentation.ViewModels
+{
+    public class MyProfilePageViewModelTest
+    {
+        #region EditCommand
+
+        [Test]
+        public void EditCommand_ShoulldNavigateToEditProfilePage()
+        {
+            var navigationService = Mock.NavigationService();
+            var viewModel = new MyProfilePageViewModel(navigationService.Object);
+
+            viewModel.EditCommand.Execute();
+
+            navigationService
+                .Verify(
+                    x =>
+                        x.NavigateAsync(
+                            "EditProfilePage",
+                            It.Is<NavigationParameters>(parameters =>
+                                    parameters.Count == 1
+                                &&  parameters[ParameterNames.Model] == viewModel.Model),
+                            null,
+                            true),
+                    Times.Once());
+        }
+
+        #endregion
+
+        #region SettingCommand
+
+        [Test]
+        public void SettingCommand_ShouldNavigateToSettingPage()
+        {
+            var navigationService = Mock.NavigationService();
+            var viewModel = new MyProfilePageViewModel(navigationService.Object);
+
+            viewModel.SettingCommand.Execute();
+
+            navigationService
+                .Verify(
+                    x =>
+                        x.NavigateAsync(
+                            "SettingPage",
+                            null,
+                            null,
+                            true),
+                    Times.Once());
+        }
+
+        #endregion
+
+        #region ShowFriendsCommand
+
+        [Test]
+        public void ShowFriendsCommand_ShoulldNavigateToFollowUsersPage()
+        {
+            var navigationService = Mock.NavigationService();
+            var viewModel = new MyProfilePageViewModel(navigationService.Object);
+
+            viewModel.ShowFriendsCommand.Execute();
+
+            navigationService
+                .Verify(
+                    x =>
+                        x.NavigateAsync(
+                            "FollowUsersPage",
+                            null,
+                            null,
+                            true),
+                    Times.Once());
+        }
+
+        #endregion
+    }
+}
